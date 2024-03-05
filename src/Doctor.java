a8fb09f65faf9cad96bf72fd80821fa51f55353f
Memo56-FB <memo56_fb@hotmail.com>
2024-03-04 23:52:32 -0600

    🐸 static elements

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,20 @@
 public class Doctor {
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
     //Comportamientos
+    Doctor() {
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
+    Doctor(String name) {
+        id++;
+        this.name = name;
+    }
     public void showName() {
         System.out.println(name);
+    }
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args){
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Guillermo Farfan";
+        Doctor myDoctor = new Doctor("Pablo");
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorMemo = new Doctor("Memo");
+        myDoctorMemo.showId();
+
+        showMenu();
     }
+    
 }
